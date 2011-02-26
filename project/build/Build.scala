9b2775e40f460b8f7f7e11a42f9d04c191c6abc3
Inigo <inigo.surguy@gmail.com>
2011-02-26 10:31:28 +0000

    Disable scanning for compatibility with JRebel

--- project/build/Build.scala
+++ project/build/Build.scala
@@ -2,4 +2,7 @@ import sbt._
 
 class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
   val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
+
+  // For use with JRebel 
+  override def scanDirectories = Nil
 }
