c5ac54a5ed97304c5445f46ff0fdde9ca8c6725c
Inigo <inigo.surguy@gmail.com>
2011-02-27 21:28:17 +0000

    Connected up REST API

--- src/main/scala/net/surguy/censord/RestApi.scala
+++ src/main/scala/net/surguy/censord/RestApi.scala
@@ -16,10 +16,16 @@ object RestApi extends RestHelper {
   /** Determine which requests we will respond to - this is called via the dispatch table set up in Boot. */
   serve {
     case "api" :: "check" :: _ Get _ => for {text <- S.param("text") ?~ "Param 'text' is missing"} yield checkText(text)
-    case "api" :: "add" :: _ Post _ => for {text <- S.param("phrase") ?~ "Param 'phrase' is missing"} yield addPhrase(text)
+//    case "api" :: "add" :: _ Post _ => for {text <- S.param("phrase") ?~ "Param 'phrase' is missing"} yield addPhrase(text)
   }
 
-  def checkText(text: String) = <span class="check success">{ "You supplied text of "+text }</span>
+  def checkText(text: String) = {
+    val result = new Checker(Phrase.findAll).checkText(text)
+    result match {
+      case true => <span class="check success">Success</span>
+      case false => <span class="check failure">Failure</span>
+    }
+  }
 
   def addPhrase(phrase: String) = {
     Phrase.createMultiplePhrases(phrase)
