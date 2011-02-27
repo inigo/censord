1eeec56f31c060c96ee9eb2dc815e6296f1d7d1d
Inigo <inigo.surguy@gmail.com>
2011-02-27 16:06:02 +0000

    Toggle whether users are allowed using Ajax

--- src/main/scala/net/surguy/censord/snippet/ListAllowedUser.scala
+++ src/main/scala/net/surguy/censord/snippet/ListAllowedUser.scala
@@ -5,6 +5,11 @@ import net.surguy.censord.model.AllowedUser
 import net.liftweb.util.BindHelpers._
 import net.liftweb.util.{TimeHelpers, CssBind}
 
+import _root_.net.liftweb.http.SHtml._
+import net.liftweb.http.js.JsCmds.SetHtml
+import net.liftweb.common.Logger
+import java.util.Date
+import xml.{Group, Text}
 
 /**
  * @todo Add some documentation!
@@ -13,12 +18,21 @@ import net.liftweb.util.{TimeHelpers, CssBind}
  * @created 27/02/2011 14:46
  */
 
-class ListAllowedUser {
+class ListAllowedUser extends Logger {
 
   def list(): CssBind = ".line *" #> AllowedUser.findAll.map(
     w => ".username *" #> w.username
        & ".createdAt *" #> TimeHelpers.dateFormatter.format(w.createdAt.is)
-       & ".allowed *" #> { if (w.allowed.is) "Yes" else "No" }
+       & ".allowed *" #> {
+          val id = "itemAllowed"+w.primaryKeyField.is
+          def txt() = if (w.allowed.is) Text("Yes") else Text("No")
+          List(
+            <span id={ id }>{ txt }</span>,
+            ajaxButton("Toggle", {() =>
+              w.allowed( !w.allowed.is ).save()
+              SetHtml(id, txt() )
+            }))
+          }
        & ".allowed [class+]" #> { if (w.allowed.is) "good" else "bad" }
        & ".delete [href]" #> ("delete/" + w.primaryKeyField.is)
   )
