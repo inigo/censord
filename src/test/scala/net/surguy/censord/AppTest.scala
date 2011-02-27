76feca0c69274a6fb2761bb80483e63ab7dfe449
Inigo <inigo.surguy@gmail.com>
2011-02-27 09:17:32 +0000

    Neater import

--- src/test/scala/net/surguy/censord/AppTest.scala
+++ src/test/scala/net/surguy/censord/AppTest.scala
@@ -1,10 +1,10 @@
 package net.surguy.censord
 
-import _root_.java.io.File
 import _root_.net.liftweb.util._
 import _root_.net.liftweb.common._
 import org.specs.SpecificationWithJUnit
 import scala.collection.JavaConverters._
+import java.io.{FileInputStream, File}
 
 /**
  * Application level tests.
@@ -22,7 +22,7 @@ class AppTest extends SpecificationWithJUnit {
   private def malformedFiles(file: File): List[File] = file match {
    case f if f.isDirectory => f.listFiles.map(malformedFiles).toList.flatten
    case f if f.isFile && f.exists && isXhtml(f.getName) =>
-     PCDataXmlParser(new _root_.java.io.FileInputStream(file.getAbsolutePath)) match {
+     PCDataXmlParser(new FileInputStream(f)) match {
        case Full(_) => List()
        case _ => List(file)
      }
